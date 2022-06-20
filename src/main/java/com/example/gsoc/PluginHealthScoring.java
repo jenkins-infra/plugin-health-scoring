a369859d258088c27e641cc1f150015ae3b3b83a
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2022-06-20 19:20:55 +0530

    discard use of env var, using @Value instead

--- src/main/java/com/example/gsoc/PluginHealthScoring.java
+++ src/main/java/com/example/gsoc/PluginHealthScoring.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 import com.example.gsoc.model.Plugin;
 import com.example.gsoc.service.PluginService;
@@ -22,11 +22,8 @@ public class PluginHealthScoring implements CommandLineRunner {
 	@Autowired
 	private PluginService pluginService;
 
-	private final Environment env;
-
-	public PluginHealthScoring(Environment env) {
-		this.env = env;
-	}
+	@Value("${jenkins.update.center}")
+	private String updateCenterURL;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PluginHealthScoring.class, args);
@@ -34,9 +31,7 @@ public class PluginHealthScoring implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws StreamReadException, DatabindException, IOException {
-		List<Plugin> pluginList = pluginService.readUpdateCenter(env.getProperty("jenkins.update.center"));
-	
-		pluginService.readUpdateCenter(env.getProperty("jenkins.update.center"));
+		List<Plugin> pluginList = pluginService.readUpdateCenter(updateCenterURL);
 
 		for (Plugin plugin : pluginList) {			
 			pluginService.saveOrUpdate(plugin);
