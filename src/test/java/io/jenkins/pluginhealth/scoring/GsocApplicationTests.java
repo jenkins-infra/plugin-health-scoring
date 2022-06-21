8d20a07697213eea5c8b936d78831d6526c3cbd1
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2022-06-21 16:14:32 +0200

    Matches the application name and its test

--- src/test/java/io/jenkins/pluginhealth/scoring/GsocApplicationTests.java
+++ src/test/java/io/jenkins/pluginhealth/scoring/GsocApplicationTests.java
@@ -28,7 +28,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GsocApplicationTests {
+class PluginHealthScoringTests {
 
 	@Test
 	void contextLoads() {
