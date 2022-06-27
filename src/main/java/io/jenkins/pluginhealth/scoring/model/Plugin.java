c8dbfd57825bd37fff02f08277f71e082df4cf17
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2022-06-27 11:33:59 +0530

    remove .* import

--- src/main/java/io/jenkins/pluginhealth/scoring/model/Plugin.java
+++ src/main/java/io/jenkins/pluginhealth/scoring/model/Plugin.java
@@ -25,7 +25,12 @@
 package io.jenkins.pluginhealth.scoring.model;
 
 import java.time.ZonedDateTime;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "plugins")
