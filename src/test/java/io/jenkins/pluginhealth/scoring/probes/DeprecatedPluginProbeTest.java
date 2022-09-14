575dcc1644190925e1b88560f274659a034422fb
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2022-09-14 16:59:50 +0200

    Validates DeprecatedPluginProbe

---
+++ src/test/java/io/jenkins/pluginhealth/scoring/probes/DeprecatedPluginProbeTest.java
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Jenkins Infra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.jenkins.pluginhealth.scoring.probes;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Map;
+
+import io.jenkins.pluginhealth.scoring.model.Plugin;
+import io.jenkins.pluginhealth.scoring.model.ProbeResult;
+import io.jenkins.pluginhealth.scoring.model.ResultStatus;
+import io.jenkins.pluginhealth.scoring.model.UpdateCenter;
+import io.jenkins.pluginhealth.scoring.model.UpdateCenterDeprecation;
+import io.jenkins.pluginhealth.scoring.model.UpdateCenterPlugin;
+
+import org.junit.jupiter.api.Test;
+
+public class DeprecatedPluginProbeTest {
+    @Test
+    public void shouldNotRequireRelease() {
+        assertThat(new DeprecatedPluginProbe().requiresRelease()).isFalse();
+    }
+
+    @Test
+    public void shouldHaveStaticKey() {
+        assertThat(new DeprecatedPluginProbe().key()).isEqualTo("deprecation");
+    }
+
+    @Test
+    public void shouldBeAbleToDetectNonDeprecatedPlugin() {
+        final Plugin plugin = mock(Plugin.class);
+        final ProbeContext ctx = mock(ProbeContext.class);
+        final DeprecatedPluginProbe probe = new DeprecatedPluginProbe();
+
+        when(plugin.getName()).thenReturn("foo");
+        when(ctx.getUpdateCenter()).thenReturn(new UpdateCenter(
+            Map.of("foo", new UpdateCenterPlugin("foo", "scm", ZonedDateTime.now().minusDays(1), Collections.emptyList())),
+            Map.of("bar", new UpdateCenterDeprecation("find-the-reason-here"))
+        ));
+
+        final ProbeResult result = probe.apply(plugin, ctx);
+
+        assertThat(result.status()).isEqualTo(ResultStatus.SUCCESS);
+    }
+
+    @Test
+    public void shouldBeAbleToDetectDeprecatedPlugin() {
+        final Plugin plugin = mock(Plugin.class);
+        final ProbeContext ctx = mock(ProbeContext.class);
+        final DeprecatedPluginProbe probe = new DeprecatedPluginProbe();
+
+        when(plugin.getName()).thenReturn("foo");
+        when(ctx.getUpdateCenter()).thenReturn(new UpdateCenter(
+            Map.of("foo", new UpdateCenterPlugin("foo", "scm", ZonedDateTime.now().minusDays(1), Collections.emptyList())),
+            Map.of("bar", new UpdateCenterDeprecation("find-the-reason-here"), "foo", new UpdateCenterDeprecation("this-is-the-reason"))
+        ));
+
+        final ProbeResult result = probe.apply(plugin, ctx);
+
+        assertThat(result.status()).isEqualTo(ResultStatus.FAILURE);
+        assertThat(result.message()).isEqualTo("this-is-the-reason");
+    }
+}
