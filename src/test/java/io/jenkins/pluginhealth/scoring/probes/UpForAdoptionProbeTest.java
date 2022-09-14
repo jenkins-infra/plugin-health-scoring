8225577afe978c052289544c9ec9724a3fc6c0df
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2022-09-14 17:17:03 +0200

    Removes unused and incorrect imports

--- src/test/java/io/jenkins/pluginhealth/scoring/probes/UpForAdoptionProbeTest.java
+++ src/test/java/io/jenkins/pluginhealth/scoring/probes/UpForAdoptionProbeTest.java
@@ -24,7 +24,6 @@
 
 package io.jenkins.pluginhealth.scoring.probes;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
