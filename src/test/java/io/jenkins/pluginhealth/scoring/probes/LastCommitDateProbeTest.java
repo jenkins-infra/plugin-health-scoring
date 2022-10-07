24694eb207034887498e0d6b913525f02246f6aa
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2022-10-07 08:46:10 +0200

    Provides temporary directory to clone plugin repo

--- src/test/java/io/jenkins/pluginhealth/scoring/probes/LastCommitDateProbeTest.java
+++ src/test/java/io/jenkins/pluginhealth/scoring/probes/LastCommitDateProbeTest.java
@@ -4,7 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Map;
+import java.util.UUID;
 
 import io.jenkins.pluginhealth.scoring.model.Plugin;
 import io.jenkins.pluginhealth.scoring.model.ProbeResult;
@@ -32,14 +35,14 @@ class LastCommitDateProbeTest {
     }
 
     @Test
-    public void shouldReturnSuccessStatusOnValidSCM() {
+    public void shouldReturnSuccessStatusOnValidSCM() throws IOException {
         final Plugin plugin = mock(Plugin.class);
         final ProbeContext ctx = mock(ProbeContext.class);
         final LastCommitDateProbe probe = new LastCommitDateProbe();
 
         when(plugin.getDetails()).thenReturn(Map.of(SCMLinkValidationProbe.KEY, ProbeResult.success("scm", "The plugin SCM link is valid")));
         when(plugin.getScm()).thenReturn("https://github.com/jenkinsci/parameterized-trigger-plugin.git");
-        when(plugin.getName()).thenReturn("parameterized-trigger");
+        when(ctx.getScmRepository()).thenReturn(Files.createTempDirectory(UUID.randomUUID().toString()));
         final ProbeResult r = probe.apply(plugin, ctx);
 
         assertThat(r.id()).isEqualTo("last-commit-date");
@@ -47,14 +50,14 @@ class LastCommitDateProbeTest {
     }
 
     @Test
-    public void shouldReturnSuccessStatusOnValidSCMWithSubFolder() {
+    public void shouldReturnSuccessStatusOnValidSCMWithSubFolder() throws IOException {
         final Plugin plugin = mock(Plugin.class);
         final ProbeContext ctx = mock(ProbeContext.class);
         final LastCommitDateProbe probe = new LastCommitDateProbe();
 
         when(plugin.getDetails()).thenReturn(Map.of(SCMLinkValidationProbe.KEY, ProbeResult.success("scm", "The plugin SCM link is valid")));
         when(plugin.getScm()).thenReturn("https://github.com/jenkinsci/aws-java-sdk-plugin/aws-java-sdk-logs");
-        when(plugin.getName()).thenReturn("aws-java-sdk-logs");
+        when(ctx.getScmRepository()).thenReturn(Files.createTempDirectory(UUID.randomUUID().toString()));
         final ProbeResult r = probe.apply(plugin, ctx);
 
         assertThat(r.id()).isEqualTo("last-commit-date");
