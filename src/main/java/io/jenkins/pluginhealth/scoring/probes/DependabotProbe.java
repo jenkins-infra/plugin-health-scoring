51878461796fd71514fcdff41ea9849d5259ee62
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2022-10-11 09:21:25 +0200

    Removes unused import on DependabotProbe

--- src/main/java/io/jenkins/pluginhealth/scoring/probes/DependabotProbe.java
+++ src/main/java/io/jenkins/pluginhealth/scoring/probes/DependabotProbe.java
@@ -3,7 +3,6 @@ package io.jenkins.pluginhealth.scoring.probes;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import io.jenkins.pluginhealth.scoring.model.Plugin;
