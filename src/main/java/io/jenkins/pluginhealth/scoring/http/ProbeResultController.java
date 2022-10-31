bb3aaeba7e975b87b4b760519fa51ef21431f78e
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2022-10-31 22:26:36 +0530

    Adds a new view at "/results" which lists the results of probes

---
+++ src/main/java/io/jenkins/pluginhealth/scoring/http/ProbeResultController.java
@@ -0,0 +1,54 @@
+package io.jenkins.pluginhealth.scoring.http;
+
+import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.jenkins.pluginhealth.scoring.model.ResultStatus;
+import io.jenkins.pluginhealth.scoring.service.PluginService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(path = "/results")
+public class ProbeResultController {
+
+    private final PluginService pluginService;
+
+    public ProbeResultController(PluginService pluginService) {
+        this.pluginService = pluginService;
+    }
+
+    @GetMapping(path = "")
+    public ModelAndView list() {
+        final ModelAndView modelAndView = new ModelAndView("probes/results");
+
+        modelAndView.addObject(
+            "probeResults",
+            getProbeResultsData()
+        );
+
+        return modelAndView;
+    }
+
+    @Transactional
+    public Map<String, Integer> getProbeResultsData() {
+        Map<String, Integer> probeResultsMap = new HashMap<>();
+
+        pluginService.streamAll().forEach(
+            plugin -> {
+                plugin.getDetails().values().forEach(probeResult -> {
+                    if (probeResult.status() == ResultStatus.SUCCESS) {
+                        probeResultsMap.put(probeResult.id(), probeResultsMap
+                            .containsKey(probeResult.id()) ? probeResultsMap
+                            .get(probeResult.id()) + 1 : 1);
+                    }
+                });
+            });
+
+        return probeResultsMap;
+    }
+}
