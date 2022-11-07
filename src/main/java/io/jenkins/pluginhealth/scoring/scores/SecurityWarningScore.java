d7bbb12254e6140ab1eace07f937bb077901b911
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2022-11-07 17:30:03 +0100

    Scores plugins based on public security advisory

---
+++ src/main/java/io/jenkins/pluginhealth/scoring/scores/SecurityWarningScore.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Jenkins Infra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.jenkins.pluginhealth.scoring.scores;
+
+import io.jenkins.pluginhealth.scoring.model.Plugin;
+import io.jenkins.pluginhealth.scoring.model.ProbeResult;
+import io.jenkins.pluginhealth.scoring.model.ResultStatus;
+import io.jenkins.pluginhealth.scoring.model.ScoreResult;
+import io.jenkins.pluginhealth.scoring.probes.KnownSecurityVulnerabilityProbe;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityWarningScore extends Scoring {
+    private final static float COEFFICIENT = 1f;
+    private final static String KEY = "security";
+
+    @Override
+    protected ScoreResult doApply(Plugin plugin) {
+        final ProbeResult securityProbeResult = plugin.getDetails().get(KnownSecurityVulnerabilityProbe.KEY);
+        if (securityProbeResult != null && securityProbeResult.status().equals(ResultStatus.FAILURE)) {
+            return new ScoreResult(KEY, 0, COEFFICIENT);
+        }
+
+        return new ScoreResult(KEY, 1, COEFFICIENT);
+    }
+}
