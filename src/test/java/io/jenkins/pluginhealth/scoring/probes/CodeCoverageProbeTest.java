7aefd93179f730b8813713cc3b8f69a195baf50a
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-02-01 13:05:40 +0100

    Improves probe validation

--- src/test/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbeTest.java
+++ src/test/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbeTest.java
@@ -25,10 +25,26 @@
 package io.jenkins.pluginhealth.scoring.probes;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import io.jenkins.pluginhealth.scoring.model.Plugin;
+import io.jenkins.pluginhealth.scoring.model.ProbeResult;
+import io.jenkins.pluginhealth.scoring.model.updatecenter.UpdateCenter;
+
+import hudson.util.VersionNumber;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,5 +67,64 @@ class CodeCoverageProbeTest {
     @Test
     public void shouldBeRelatedToCode() {
         assertThat(spy(CodeCoverageProbe.class).isSourceCodeRelated()).isTrue();
+    }
+
+    @Test
+    public void shouldRequireJenkinsfile() {
+        final Plugin plugin = mock(Plugin.class);
+        final ProbeContext ctx = mock(ProbeContext.class);
+
+        when(plugin.getDetails()).thenReturn(Map.of(
+            JenkinsfileProbe.KEY, ProbeResult.failure(JenkinsfileProbe.KEY, "")
+        ));
+
+        final CodeCoverageProbe probe = new CodeCoverageProbe();
+        final ProbeResult result = probe.apply(plugin, ctx);
+
+        assertThat(result)
+            .usingRecursiveComparison()
+            .comparingOnlyFields("id", "status", "message")
+            .isEqualTo(ProbeResult.error(CodeCoverageProbe.KEY, "Requires Jenkinsfile"));
+    }
+
+    @Test
+    public void shouldBeAbleToRetrieveDetailsFromGitHubChecks() throws IOException {
+        final String pluginName = "mailer";
+        final String scmLink = "https://github.com/jenkinsci/mailer-plugin";
+        final String defaultBranch = "main";
+
+        final Plugin plugin = mock(Plugin.class);
+        final ProbeContext ctx = mock(ProbeContext.class);
+
+        final GitHub gh = mock(GitHub.class);
+        final GHRepository ghRepository = mock(GHRepository.class);
+
+        when(plugin.getName()).thenReturn(pluginName);
+        when(plugin.getDetails()).thenReturn(Map.of(
+            JenkinsfileProbe.KEY, ProbeResult.failure(JenkinsfileProbe.KEY, "")
+        ));
+        when(plugin.getScm()).thenReturn(scmLink);
+        when(ctx.getUpdateCenter()).thenReturn(new UpdateCenter(
+            Map.of(
+                pluginName, new io.jenkins.pluginhealth.scoring.model.updatecenter.Plugin(
+                    pluginName, new VersionNumber("1.0"), scmLink, ZonedDateTime.now(), List.of(), 0,
+                    "42", defaultBranch
+                )
+            ),
+            Map.of(),
+            List.of()
+        ));
+        when(ctx.getRepositoryName(plugin.getScm())).thenReturn(Optional.of("jenkinsci/mailer-plugin"));
+
+        when(gh.getRepository(anyString())).thenReturn(ghRepository);
+        when(ghRepository.getCheckRuns(defaultBranch)).thenReturn(null);
+
+        final CodeCoverageProbe probe = new CodeCoverageProbe();
+        final ProbeResult result = probe.apply(plugin, ctx);
+
+        assertThat(result)
+            .usingRecursiveComparison()
+            .comparingOnlyFields("id", "status", "message")
+            .isEqualTo(ProbeResult.error(CodeCoverageProbe.KEY, "Requires Jenkinsfile"));
     }
 }
