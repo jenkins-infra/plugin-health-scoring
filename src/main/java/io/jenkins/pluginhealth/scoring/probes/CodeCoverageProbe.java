274e727b87a18c3e86df5f9a5440ec2d5580b48f
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-02-01 13:06:33 +0100

    Using Checks API to retrive code coverage if available

--- src/main/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbe.java
+++ src/main/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbe.java
@@ -24,16 +24,58 @@
 
 package io.jenkins.pluginhealth.scoring.probes;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 import io.jenkins.pluginhealth.scoring.model.Plugin;
 import io.jenkins.pluginhealth.scoring.model.ProbeResult;
+import io.jenkins.pluginhealth.scoring.model.ResultStatus;
 
+import org.kohsuke.github.GHCheckRun;
+import org.kohsuke.github.GHRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(CodeCoverageProbe.ORDER)
 public class CodeCoverageProbe extends Probe {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CodeCoverageProbe.class);
+
     public static final String KEY = "code-coverage";
     public static final int ORDER = JenkinsfileProbe.ORDER + 100;
 
     @Override
     protected ProbeResult doApply(Plugin plugin, ProbeContext context) {
-        return null;
+        final ProbeResult jenkinsFileResult = plugin.getDetails().get(JenkinsfileProbe.KEY);
+        if (jenkinsFileResult == null || !jenkinsFileResult.status().equals(ResultStatus.SUCCESS)) {
+            return ProbeResult.error(key(), "Requires Jenkinsfile");
+        }
+
+        final io.jenkins.pluginhealth.scoring.model.updatecenter.Plugin ucPlugin =
+            context.getUpdateCenter().plugins().get(plugin.getName());
+        final String defaultBranch = ucPlugin.defaultBranch();
+        try {
+            final Optional<String> repositoryName = context.getRepositoryName(plugin.getScm());
+            if (repositoryName.isPresent()) {
+                final GHRepository ghRepository = context.getGitHub().getRepository(repositoryName.get());
+                final List<GHCheckRun> ghCheckRuns =
+                    ghRepository.getCheckRuns(defaultBranch/*, Map.of("check_name", "Code Coverage")*/).toList();
+                // Requires pull request on hub4j/github-api#TODO
+                if (ghCheckRuns.size() != 1) {
+                    return ProbeResult.failure(key(), "Could not determine code coverage for plugin");
+                } else {
+                    return ProbeResult.success(key(), ghCheckRuns.get(0).getOutput().getTitle());
+                }
+            } else {
+                return ProbeResult.failure(key(), "Plugin has no default branch configured. See Update-Center.");
+            }
+        } catch (IOException e) {
+            LOGGER.warn("Could not get Coverage check for {}", plugin.getName(), e);
+            return ProbeResult.error(key(), "Could not get coverage check");
+        }
     }
 
     @Override
