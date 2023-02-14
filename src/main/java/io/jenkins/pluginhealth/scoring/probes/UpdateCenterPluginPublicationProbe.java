9c36db8a0b7b7b9d94c5b143d3c5d2ae9efe46c4
Jagruti Tiwari <tiwarijagruti1@gmail.com>
2023-02-14 11:19:04 +0530

    Detecting when a plugin is not published in the Update Center anymore (#205)
    Co-authored-by: Jagruti Tiwari <jagrutit@cdac.in>
    Co-authored-by: Adrien Lecharpentier <alecharpentier@cloudbees.com>

---
+++ src/main/java/io/jenkins/pluginhealth/scoring/probes/UpdateCenterPluginPublicationProbe.java
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Jenkins Infra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.jenkins.pluginhealth.scoring.probes;
+
+import io.jenkins.pluginhealth.scoring.model.Plugin;
+import io.jenkins.pluginhealth.scoring.model.ProbeResult;
+import io.jenkins.pluginhealth.scoring.model.updatecenter.UpdateCenter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Check if the plugin exists in UpdateCenter map
+ *
+ * */
+@Component
+@Order(UpdateCenterPluginPublicationProbe.ORDER)
+public class UpdateCenterPluginPublicationProbe extends Probe {
+    public static final String KEY = "update-center-plugin-publication-probe";
+    public static final int ORDER = DeprecatedPluginProbe.ORDER + 1;
+
+    @Override
+    public ProbeResult doApply(Plugin plugin, ProbeContext ctx) {
+        final UpdateCenter updateCenter = ctx.getUpdateCenter();
+        final io.jenkins.pluginhealth.scoring.model.updatecenter.Plugin updateCenterPlugin = updateCenter.plugins().get(plugin.getName());
+
+        if (updateCenterPlugin == null) {
+            return ProbeResult.failure(key(), "This plugin does not exists in update-center");
+        }
+
+        return ProbeResult.success(key(), "This plugin exists in update-center");
+    }
+
+    @Override
+    public String key() {
+        return KEY;
+    }
+
+    @Override
+    public String getDescription() {
+        return "This probe detects if a specified plugin exists in update-center.";
+    }
+}
+
