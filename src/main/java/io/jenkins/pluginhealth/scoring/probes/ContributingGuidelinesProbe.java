a5437eb7cf5eb8e70cf4dc0cd99da953b4c55d9d
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-02-22 13:38:48 +0100

    Fixing code indentation

--- src/main/java/io/jenkins/pluginhealth/scoring/probes/ContributingGuidelinesProbe.java
+++ src/main/java/io/jenkins/pluginhealth/scoring/probes/ContributingGuidelinesProbe.java
@@ -41,25 +41,24 @@ import org.springframework.stereotype.Component;
 @Order(value = ContributingGuidelinesProbe.ORDER)
 public class ContributingGuidelinesProbe extends Probe {
     private static final Logger LOGGER = LoggerFactory.getLogger(ContributingGuidelinesProbe.class);
+
     public static final int ORDER = JenkinsfileProbe.ORDER + 1;
     public static final String KEY = "contributing-guidelines";
 
     @Override
     protected ProbeResult doApply(Plugin plugin, ProbeContext context) {
         if (plugin.getDetails().get(SCMLinkValidationProbe.KEY) == null) {
-            LOGGER.error("Couldn't run {} on {} because previous SCMLinkValidationProbe has null value in database",
-                    key(), plugin.getName());
+            LOGGER.error("Couldn't run {} on {} because previous SCMLinkValidationProbe has null value in database", key(), plugin.getName());
             return ProbeResult.error(key(), "SCM link has not been validated yet");
         }
         final Path repository = context.getScmRepository();
-        try (Stream<Path> paths = Files
-                .find(repository, 4,
-                        (file, basicFileAttributes) -> Files.isReadable(file)
-                                && ("CONTRIBUTING.md".equalsIgnoreCase(file.getFileName().toString())
-                                        || "CONTRIBUTING.adoc".equalsIgnoreCase(file.getFileName().toString())))) {
+        try (Stream<Path> paths = Files.find(repository, 2,
+            (file, basicFileAttributes) -> Files.isReadable(file)
+                && ("CONTRIBUTING.md".equalsIgnoreCase(file.getFileName().toString())
+                || "CONTRIBUTING.adoc".equalsIgnoreCase(file.getFileName().toString())))) {
             return paths.findFirst()
-                    .map(file -> ProbeResult.success(key(), "Contributing guidelines found"))
-                    .orElseGet(() -> ProbeResult.failure(key(), "No contributing guidelines found"));
+                .map(file -> ProbeResult.success(key(), "Contributing guidelines found"))
+                .orElseGet(() -> ProbeResult.failure(key(), "No contributing guidelines found"));
         } catch (IOException e) {
             return ProbeResult.error(key(), e.getMessage());
         }
@@ -72,9 +71,7 @@ public class ContributingGuidelinesProbe extends Probe {
 
     @Override
     public String getDescription() {
-        return """
-                Validates the existence of a `CONTRIBUTING.adoc` or `CONTRIBUTING.md` file in the repository.
-                """;
+        return "Validates the existence of a `CONTRIBUTING.adoc` or `CONTRIBUTING.md` file in the repository.";
     }
 
     @Override
