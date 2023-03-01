8a9d6e5c533769dc9eb4846dce85736a9fdbbb6d
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-03-01 11:30:59 +0100

    Unused import

--- src/test/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbeTest.java
+++ src/test/java/io/jenkins/pluginhealth/scoring/probes/CodeCoverageProbeTest.java
@@ -25,7 +25,6 @@
 package io.jenkins.pluginhealth.scoring.probes;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
