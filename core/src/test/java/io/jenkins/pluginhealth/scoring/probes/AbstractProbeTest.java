6f5a870ae0a948749072d7f38873d2315fccd95c
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-03-30 12:11:14 +0200

    Removes unnecessary commented code

--- core/src/test/java/io/jenkins/pluginhealth/scoring/probes/AbstractProbeTest.java
+++ core/src/test/java/io/jenkins/pluginhealth/scoring/probes/AbstractProbeTest.java
@@ -50,25 +50,4 @@ abstract class AbstractProbeTest<T extends Probe> {
     void shouldNotHaveNullRequirement() {
         assertThat(getSpy().getProbeResultRequirement()).isNotNull();
     }
-/*
-    @SuppressWarnings("unchecked")
-    @Test
-    void shouldValidateProbeRequirements() {
-        final Plugin plugin = mock(Plugin.class);
-        final ProbeContext ctx = mock(ProbeContext.class);
-
-        final Probe probe = getSpy();
-        final int probeRequirementsLength = probe.getProbeResultRequirement().length;
-
-        when(plugin.getDetails()).thenReturn(
-            Map.of()
-        );
-        // TODO generate required mocking to validate the requirements
-
-        final ProbeResult result = probe.apply(plugin, ctx);
-        for (int i = 0; i < Math.pow(probeRequirementsLength, probeRequirementsLength) - probeRequirementsLength; i++) {
-            assertThat(result.status()).isEqualTo(ResultStatus.ERROR);
-            verify(probe, never()).doApply(plugin, ctx);
-        }
-    }*/
 }
