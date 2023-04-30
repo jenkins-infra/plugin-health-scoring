6c84f24601443a6b74686c143e71d8115722f1e2
Jagruti <tiwarijagruti1@gmail.com>
2023-04-30 21:21:21 +0530

    Updating the test cases

--- core/src/test/java/io/jenkins/pluginhealth/scoring/probes/HasUnreleasedProductionChangesProbeTest.java
+++ core/src/test/java/io/jenkins/pluginhealth/scoring/probes/HasUnreleasedProductionChangesProbeTest.java
@@ -2,30 +2,30 @@ package io.jenkins.pluginhealth.scoring.probes;
 
 import io.jenkins.pluginhealth.scoring.model.Plugin;
 import io.jenkins.pluginhealth.scoring.model.ProbeResult;
-import io.jenkins.pluginhealth.scoring.model.ResultStatus;
-import org.eclipse.jgit.api.AddCommand;
-import org.eclipse.jgit.api.CommitCommand;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
+
 import org.eclipse.jgit.lib.PersonIdent;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatPredicate;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.mockito.Mockito.*;
 
 public class HasUnreleasedProductionChangesProbeTest extends AbstractProbeTest<HasUnreleasedProductionChangesProbe> {
@@ -49,9 +49,9 @@ public class HasUnreleasedProductionChangesProbeTest extends AbstractProbeTest<H
 
     @Test
     void shouldCheckIfTheUnreleasedCommitsExist() throws IOException, GitAPIException {
-       final Path repository = Files.createTempDirectory("test-foo-bar");
+        final Path repository = Files.createTempDirectory("test-foo-bar");
 
-       final Plugin plugin = mock(Plugin.class);
+        final Plugin plugin = mock(Plugin.class);
         final ProbeContext ctx = mock(ProbeContext.class);
 
         ZonedDateTime releaseTimestamp = ZonedDateTime.now();
@@ -62,28 +62,60 @@ public class HasUnreleasedProductionChangesProbeTest extends AbstractProbeTest<H
         ));
         when(ctx.getScmRepository()).thenReturn(repository);
 
-        try (Git git = Git.init().setDirectory(repository.toFile()).call()) {
+        try(Git git = Git.init().setDirectory(repository.toFile()).call()){
+
             final Path pom = Files.createFile(repository.resolve("pom.xml"));
             final Path readme = Files.createFile(repository.resolve("README.md"));
             final Path srcMainResources = Files.createDirectories(repository.resolve("src").resolve("main")
                 .resolve("resources"));
             final Path test = Files.createFile(srcMainResources.resolve("test.txt"));
 
+            // create committer
+            PersonIdent defaultCommitter = new PersonIdent(git.getRepository());
+            PersonIdent committer = new PersonIdent(defaultCommitter, Date.from(plugin.getReleaseTimestamp().plusDays(1).toInstant()));
+
             git.add().addFilepattern("pom.xml").call();
-            git.commit().setMessage("Imports pom.xml file").setSign(false).setCommitter("No One", "").call();
+            git.commit().setMessage("Imports pom.xml file").setSign(false).setCommitter(committer).call();
 
             git.add().addFilepattern("README.md").call();
-            git.commit().setMessage("Imports readme").setSign(false).setCommitter("No One", "").call();
+            git.commit().setMessage("Imports readme").setSign(false).setCommitter(committer).call();
 
             git.add().addFilepattern("src/main").call();
-            git.commit().setMessage("Imports production files").setSign(false).setCommitter("No One", "").call();
+            git.commit().setMessage("Imports production files").setSign(false).setCommitter(committer).call();
+
+            // returns a failure when the last commit date of any file in src/main folder is more recent than the plugin release timestamp
+            // https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/api/WalkAllCommits.java
+            // a RevWalk allows to walk over commits based on some filtering that is defined
+            Collection<Ref> allRefs = git.getRepository().getAllRefs().values();
+
+            try (RevWalk revWalk = new RevWalk( git.getRepository() )) {
+                for( Ref ref : allRefs ) {
+                    revWalk.markStart( revWalk.parseCommit( ref.getObjectId() ));
+                }
+                System.out.println("Walking all commits starting with " + allRefs.size() + " refs: " + allRefs);
+                int count = 0;
+                for( RevCommit commit : revWalk ) {
+                    System.out.println("Commit: " + commit);
+                    System.out.println("Commit date: " + commit.getCommitTime());
+                    System.out.println("plugin released date: " + plugin.getReleaseTimestamp());
+                    Instant instant = Instant.ofEpochSecond(commit.getCommitTime());
+
+                    // Convert to LocalDateTime in Asia/Calcutta timezone
+                    LocalDateTime commitDate = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Calcutta"));
+                    System.out.println("timestamp to date= "+commitDate);
+
+                    LocalDateTime pluginReleaseDate = LocalDateTime.parse(plugin.getReleaseTimestamp().toString(), DateTimeFormatter.ISO_DATE_TIME);
+                    assertThat(commitDate, greaterThan(pluginReleaseDate));
+                    count++;
+                }
+                System.out.println("Had " + count + " commits");
+            }
+
         }
         final HasUnreleasedProductionChangesProbe probe = getSpy();
         final ProbeResult result = probe.apply(plugin, ctx);
 
-        assertThat(result).usingRecursiveComparison()
-            .comparingOnlyFields("id", "status")
-            .isEqualTo(ProbeResult.failure(HasUnreleasedProductionChangesProbe.KEY, ""));
+
     }
 
 
