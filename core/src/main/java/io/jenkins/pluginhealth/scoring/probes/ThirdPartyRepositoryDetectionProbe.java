491d16d497a8dcd91cb3dc2618e85eab05d1f93f
Jagruti <tiwarijagruti1@gmail.com>
2023-06-03 20:29:23 +0530

    Updating the code and test cases

--- core/src/main/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbe.java
+++ core/src/main/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbe.java
@@ -2,11 +2,19 @@ package io.jenkins.pluginhealth.scoring.probes;
 
 import io.jenkins.pluginhealth.scoring.model.Plugin;
 import io.jenkins.pluginhealth.scoring.model.ProbeResult;
-
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Repository;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 @Component
 @Order(value = ThirdPartyRepositoryDetectionProbe.ORDER)
@@ -18,7 +26,31 @@ public class ThirdPartyRepositoryDetectionProbe extends Probe{
 
     @Override
     protected ProbeResult doApply(Plugin plugin, ProbeContext context) {
-        return null;
+        final String path = "https://repo.jenkins-ci.org";
+        MavenXpp3Reader mavenReader = new MavenXpp3Reader();
+        try {
+            Model model = mavenReader.read(new FileReader(plugin.getScm()+"/pom.xml"));
+            for (Repository repository : getRepositories(model)) {
+                if(repository.getUrl().startsWith(path)) {
+                    return ProbeResult.failure(KEY, "Third party repository detected in the plugin");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ex 1");
+            return ProbeResult.error(KEY, e.getMessage());
+        } catch (XmlPullParserException e) {
+            System.out.println("ex 2");
+            return ProbeResult.error(KEY, e.getMessage());
+        } catch (IOException e) {
+            System.out.println("ex 3");
+            return ProbeResult.error(KEY, e.getMessage());
+
+        }
+        return ProbeResult.success(KEY, "The plugin has no third party repositories");
+    }
+
+    private static List<Repository> getRepositories(Model model) {
+        return model.getRepositories();
     }
 
     @Override
--- core/src/test/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbeTest.java
+++ core/src/test/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbeTest.java
@@ -2,15 +2,24 @@ package io.jenkins.pluginhealth.scoring.probes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
 
 import io.jenkins.pluginhealth.scoring.model.Plugin;
 import io.jenkins.pluginhealth.scoring.model.ProbeResult;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Repository;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.jupiter.api.Test;
 
 public class ThirdPartyRepositoryDetectionProbeTest extends AbstractProbeTest<ThirdPartyRepositoryDetectionProbe> {
@@ -20,21 +29,23 @@ public class ThirdPartyRepositoryDetectionProbeTest extends AbstractProbeTest<Th
     }
 
     @Test
-    void shouldFailIfThirdPartRepositoriesDetected() {
-
+    void shouldFailIfThirdPartRepositoriesDetected() throws IOException, XmlPullParserException {
         final Plugin plugin = mock(Plugin.class);
         final ProbeContext ctx = mock(ProbeContext.class);
-        final ThirdPartyRepositoryDetectionProbe probe = getSpy();
 
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("fake-test-directory/pom.xml").getFile());
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("pom-test-both-paths/pom.xml");
+
+        MavenXpp3Reader mavenReader = mock(MavenXpp3Reader.class);
+        Model mockModel = mock(Model.class);
+        List<Repository> repositoryList = mavenReader.read(inputStream).getRepositories();
+        when(mockModel.getRepositories()).thenReturn(repositoryList);
+
+        final ThirdPartyRepositoryDetectionProbe probe = getSpy();
 
         assertThat(probe.apply(plugin, ctx))
             .usingRecursiveComparison()
             .comparingOnlyFields("id", "message", "status")
             .isEqualTo(ProbeResult.failure(ThirdPartyRepositoryDetectionProbe.KEY, "Third party repository detected in the plugin"));
-        verify(probe).doApply(any(Plugin.class), any(ProbeContext.class));
-
     }
 
     @Test
