9d43283ea31baef6a61d97877d5fd2fd60be0bdf
Jagruti <tiwarijagruti1@gmail.com>
2023-06-13 22:13:17 +0530

    Removing unused imports

--- core/src/main/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbe.java
+++ core/src/main/java/io/jenkins/pluginhealth/scoring/probes/ThirdPartyRepositoryDetectionProbe.java
@@ -2,14 +2,11 @@ package io.jenkins.pluginhealth.scoring.probes;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
