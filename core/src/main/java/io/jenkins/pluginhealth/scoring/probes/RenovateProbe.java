c41a032cb4a6113eec120b435516ff7e183ce0b8
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-08-08 18:05:54 +0200

    Creates method to get dependency bot name

--- core/src/main/java/io/jenkins/pluginhealth/scoring/probes/RenovateProbe.java
+++ core/src/main/java/io/jenkins/pluginhealth/scoring/probes/RenovateProbe.java
@@ -4,17 +4,16 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
-* Looks for Renovate bot configuration in a plugin.
-*
-* @see io.jenkins.pluginhealth.scoring.probes.AbstractDependencyBotConfigurationProbe
-*/
+ * Looks for Renovate bot configuration in a plugin.
+ */
 @Component
 @Order(AbstractDependencyBotConfigurationProbe.ORDER)
 public class RenovateProbe extends AbstractDependencyBotConfigurationProbe {
     public static final String KEY = "renovate";
 
-    RenovateProbe() {
-        super(KEY);
+    @Override
+    protected String getBotName() {
+        return KEY;
     }
 
     @Override
