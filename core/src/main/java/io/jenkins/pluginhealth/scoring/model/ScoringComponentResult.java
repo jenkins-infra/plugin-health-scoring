530a8e5c812f6430c26be3e2e7bb83c6ebc2b0d7
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2024-02-01 10:12:31 +0100

    Introduces field to store resolutions for poor scores results

--- core/src/main/java/io/jenkins/pluginhealth/scoring/model/ScoringComponentResult.java
+++ core/src/main/java/io/jenkins/pluginhealth/scoring/model/ScoringComponentResult.java
@@ -31,9 +31,13 @@ import io.jenkins.pluginhealth.scoring.scores.ScoringComponent;
 /**
  * Describes the evaluation from a {@link ScoringComponent} on a specific plugin.
  *
- * @param score   the score representing the points granted to the plugin, out of 100 (one hundred).
- * @param weight  the weight of the score
- * @param reasons the list of string explaining the score granted to the plugin
+ * @param score       the score representing the points granted to the plugin, out of 100 (one hundred).
+ * @param weight      the weight of the score
+ * @param reasons     the list of string explaining the score granted to the plugin
+ * @param resolutions the list of link providing help to increase the score
  */
-public record ScoringComponentResult(int score, float weight, List<String> reasons) {
+public record ScoringComponentResult(int score, float weight, List<String> reasons, List<String> resolutions) {
+    public ScoringComponentResult(int score, float weight, List<String> reasons) {
+        this(score, weight, reasons, List.of());
+    }
 }
