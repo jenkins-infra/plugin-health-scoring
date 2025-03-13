e9035c90c8353208d40883982bf13249c70d1548
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2025-03-13 11:43:52 +0100

    Creates release script to facilitate knowledge transfer

---
+++ release.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+# This script is used to generate the project releases.
+# This uses 'git' 'gh', 'jq', 'mvn' and must be used with a Bash version greater than or equal to 4.
+
+cmds=('git' 'gh' 'jq' 'mvn')
+for cmd in "${cmds[@]}"; do
+    command -v "${cmd}" &> /dev/null
+done
+
+if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+	echo "You **must** at least run a Bash version 4. You are using bash version ${BASH_VERSINFO[0]}."
+	echo "If you are on a mac, use brew to install bash and use '$(brew --prefix bash)/bin/bash $0 $*'"
+	exit 1
+fi
+
+draft_release=$(\
+    gh release list --json tagName,isDraft\
+    | jq '[.[] | select(.isDraft == true)]'\
+)
+
+if [ "$(echo "${draft_release}" | jq '. | length')" -eq 0 ]; then
+    echo "# There is no release in draft on GitHub. Please assess."
+    exit 1
+fi
+if [ "$(echo "${draft_release}" | jq '. | length')" -gt 1 ]; then
+    echo "# There is too many releases in draft on GitHub. Please assess."
+    exit 1
+fi
+
+gh_draft_tag=$(\
+    echo "${draft_release}"\
+    | jq --raw-output '.[0].tagName'
+)
+
+if [ ${#@} -eq 1 ] ; then
+    version=$1
+    shift
+else
+    echo "# Using GitHub Release to get the release version."
+    version=${gh_draft_tag/#v/}
+fi
+
+echo "# Release version will be ${version}."
+
+mvn release:prepare\
+  -DreleaseVersion="${version}"\
+  -B\
+  -DskipTests\
+  -Dspotbugs.skip=true\
+  -Dspotless.check.skip=true\
+  -Darguments="-DskipTests -Dspotbugs.skip=true -Dspotless.check.skip=true"\
+  -DlocalCheckout=true\
+  -DdeployAtEnd=true\
+  -DretryFailedDeploymentCount=3
+
+declare -A props
+while IFS='=' read -r key value; do
+	props["${key}"]="${value}"
+done < <(grep -v "^[[:space:]]*#" "release.properties")
+
+release_tag="${props["scm.tag"]}"
+release_title="${release_tag/#v/}"
+
+gh release edit "${gh_draft_tag}"\
+    --verify-tag\
+    --tag "v${release_tag}"\
+    --title="${release_title}"\
+    --draft=false\
+    --latest
+
+mvn -q release:clean
