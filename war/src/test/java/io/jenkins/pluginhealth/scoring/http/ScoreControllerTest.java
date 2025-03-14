3aa20b8d75a3fbafdcd753d99d32ab8afa6eada9
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2025-03-14 08:29:36 +0100

    Removes existing todo and project configuration issues (#602)

--- war/src/test/java/io/jenkins/pluginhealth/scoring/http/ScoreControllerTest.java
+++ war/src/test/java/io/jenkins/pluginhealth/scoring/http/ScoreControllerTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Jenkins Infra
+ * Copyright (c) 2023-2025 Jenkins Infra
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,10 +21,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package io.jenkins.pluginhealth.scoring.http;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -34,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.time.ZonedDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -57,57 +60,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @ImportAutoConfiguration({ProjectInfoAutoConfiguration.class, SecurityConfiguration.class})
-@WebMvcTest(
-    controllers = ScoreController.class
-)
+@WebMvcTest(controllers = ScoreController.class)
 class ScoreControllerTest {
-    @MockBean private ScoreService scoreService;
-    @MockBean private ScoringService scoringService;
-    @Autowired private MockMvc mockMvc;
+    @MockitoBean
+    private ScoreService scoreService;
+
+    @MockitoBean
+    private ScoringService scoringService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void shouldDisplayListOfScoring() throws Exception {
-        when(scoringService.getScoringList())
-            .thenReturn(List.of(
-                new Scoring() {
-                    @Override
-                    public String key() {
-                        return "foo";
-                    }
+        when(scoringService.getScoringList()).thenReturn(List.of(new Scoring() {
+            @Override
+            public String key() {
+                return "foo";
+            }
 
-                    @Override
-                    public float weight() {
-                        return 1;
-                    }
+            @Override
+            public float weight() {
+                return 1;
+            }
 
-                    @Override
-                    public String description() {
-                        return "description";
-                    }
+            @Override
+            public String description() {
+                return "description";
+            }
 
-                    @Override
-                    public List<ScoringComponent> getComponents() {
-                        return List.of();
-                    }
+            @Override
+            public List<ScoringComponent> getComponents() {
+                return List.of();
+            }
 
-                    @Override
-                    public int version() {
-                        return 0;
-                    }
-                }
-            ));
+            @Override
+            public int version() {
+                return 0;
+            }
+        }));
 
         mockMvc.perform(get("/scores"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("scores/listing"))
-            .andExpect(model().attribute("module", "scores"))
-            .andExpect(model().attributeExists("scorings"));
+                .andExpect(status().isOk())
+                .andExpect(view().name("scores/listing"))
+                .andExpect(model().attribute("module", "scores"))
+                .andExpect(model().attributeExists("scorings"));
     }
 
     @Test
@@ -118,9 +121,7 @@ class ScoreControllerTest {
 
         final Plugin plugin = mock(Plugin.class);
         when(plugin.getName()).thenReturn(pluginName);
-        when(plugin.getDetails()).thenReturn(Map.of(
-            probeKey, ProbeResult.success(probeKey, "message", 1)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of(probeKey, ProbeResult.success(probeKey, "message", 1)));
         when(plugin.getScm()).thenReturn("this-is-the-url-of-the-plugin-location");
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusHours(1));
         when(plugin.getVersion()).thenReturn(new VersionNumber("1.0"));
@@ -128,37 +129,35 @@ class ScoreControllerTest {
         final Score score = mock(Score.class);
         when(score.getPlugin()).thenReturn(plugin);
         when(score.getValue()).thenReturn(42L);
-        when(score.getDetails()).thenReturn(Set.of(
-            new ScoreResult(scoreKey, 42, 1f, Set.of(), 1)
-        ));
+        when(score.getDetails()).thenReturn(Set.of(new ScoreResult(scoreKey, 42, 1f, Set.of(), 1)));
 
         when(scoreService.latestScoreFor(pluginName)).thenReturn(Optional.of(score));
 
         mockMvc.perform(get("/scores/{pluginName}", pluginName))
-            .andExpect(status().isOk())
-            .andExpect(view().name("scores/details"))
-            .andExpect(model().attribute("module", "scores"))
-            .andExpect(model().attributeExists("score"))
-            /*.andExpect(model().attribute(
-                "score",
-                allOf(
-                    hasProperty("value", equalTo(42L)),
-                    hasProperty("pluginName", equalTo(pluginName)),
-                    hasProperty("probeResults", instanceOf(Map.class)),
-                    hasProperty("details", instanceOf(Collection.class))
-                )
-            ))*/; // TODO see https://github.com/hamcrest/JavaHamcrest/issues/392
+                .andExpect(status().isOk())
+                .andExpect(view().name("scores/details"))
+                .andExpect(model().attribute("module", "scores"))
+                .andExpect(model().attributeExists("score"))
+                .andExpect(model().attribute(
+                                "score",
+                                allOf(
+                                        hasProperty("value", equalTo(42L)),
+                                        hasProperty(
+                                                "plugin",
+                                                allOf(
+                                                        hasProperty("name", equalTo(pluginName)),
+                                                        hasProperty("details", instanceOf(Map.class)))),
+                                        hasProperty("details", instanceOf(Collection.class)))));
     }
 
     @Test
     void shouldDisplayErrorWhenPluginUnknown() throws Exception {
-        when(scoreService.latestScoreFor(anyString()))
-            .thenReturn(Optional.empty());
+        when(scoreService.latestScoreFor(anyString())).thenReturn(Optional.empty());
 
         mockMvc.perform(get("/scores/foo-bar"))
-            .andExpect(status().isNotFound())
-            .andExpect(view().name("scores/unknown"))
-            .andExpect(model().attribute("module", "scores"))
-            .andExpect(model().attribute("pluginName", equalTo("foo-bar")));
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("scores/unknown"))
+                .andExpect(model().attribute("module", "scores"))
+                .andExpect(model().attribute("pluginName", equalTo("foo-bar")));
     }
 }
