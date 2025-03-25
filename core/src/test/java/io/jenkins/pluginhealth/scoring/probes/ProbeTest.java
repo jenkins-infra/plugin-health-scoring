59451bf4efb5a8ee7473eacdfde1ab311c2d7497
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2025-03-25 18:05:18 +0100

    chore: Comply to spotless

--- core/src/test/java/io/jenkins/pluginhealth/scoring/probes/ProbeTest.java
+++ core/src/test/java/io/jenkins/pluginhealth/scoring/probes/ProbeTest.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Jenkins Infra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.jenkins.pluginhealth.scoring.probes;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,9 +63,7 @@ public class ProbeTest {
         when(probe.key()).thenReturn("key");
         when(probe.getVersion()).thenReturn(2L);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
 
         assertThat(probe.isApplicable(plugin, mock(ProbeContext.class))).isTrue();
     }
@@ -55,9 +76,7 @@ public class ProbeTest {
         when(probe.key()).thenReturn("key");
         when(probe.getVersion()).thenReturn(1L);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.error("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.error("key", "", 1L)));
 
         assertThat(probe.isApplicable(plugin, mock(ProbeContext.class))).isTrue();
     }
@@ -72,9 +91,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(false);
         when(probe.requiresRelease()).thenReturn(false);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
 
         assertThat(probe.isApplicable(plugin, mock(ProbeContext.class))).isTrue();
     }
@@ -89,9 +106,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(false);
         when(probe.requiresRelease()).thenReturn(true);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().plusDays(1));
 
         assertThat(probe.isApplicable(plugin, mock(ProbeContext.class))).isTrue();
@@ -108,9 +123,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(true);
         when(probe.requiresRelease()).thenReturn(true);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusDays(1));
 
         when(ctx.getScmRepository()).thenReturn(Optional.empty());
@@ -129,9 +142,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(true);
         when(probe.requiresRelease()).thenReturn(false);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusDays(1));
 
         when(ctx.getScmRepository()).thenReturn(Optional.of(tempDir.toPath()));
@@ -151,9 +162,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(false);
         when(probe.requiresRelease()).thenReturn(true);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusDays(1));
 
         when(ctx.getScmRepository()).thenReturn(Optional.of(tempDir.toPath()));
@@ -173,9 +182,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(true);
         when(probe.requiresRelease()).thenReturn(true);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusDays(1));
 
         when(ctx.getScmRepository()).thenReturn(Optional.of(tempDir.toPath()));
@@ -195,9 +202,7 @@ public class ProbeTest {
         when(probe.isSourceCodeRelated()).thenReturn(true);
         when(probe.requiresRelease()).thenReturn(true);
 
-        when(plugin.getDetails()).thenReturn(Map.of(
-            "key", ProbeResult.success("key", "", 1L)
-        ));
+        when(plugin.getDetails()).thenReturn(Map.of("key", ProbeResult.success("key", "", 1L)));
         when(plugin.getReleaseTimestamp()).thenReturn(ZonedDateTime.now().minusDays(1));
 
         when(ctx.getScmRepository()).thenReturn(Optional.of(tempDir.toPath()));
